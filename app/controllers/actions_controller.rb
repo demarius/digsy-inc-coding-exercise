dade5d93156caf00dbb1524ac2b5ad886b989c56
Demarius Chrite <chrite.demarius@gmail.com>
2017-04-09 15:27:54 -0700

    Cleanup

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,13 +1,9 @@
 class AgentActionsController < ApplicationController
     def new
         @new_action = AgentAction.new
-        puts @new_action.inspect
     end
     
     def create
-        puts "!!!!!!!!!!!!!!!!!!!!!!!!!"
-        puts @action
-        puts params.inspect
         action = AgentAction.create(params)
         redirect_to new_action_path
     end
